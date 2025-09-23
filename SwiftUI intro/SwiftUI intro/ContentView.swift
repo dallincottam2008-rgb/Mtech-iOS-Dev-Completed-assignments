484b7b2763f91e12c86083a4ffb96292d695fc90
Dallin J Cottam <dallinc@Dallins-MacBook-Pro.local>
2025-09-23 09:28:02 -0600

    added Swift UI labs

---
+++ SwiftUI intro/SwiftUI intro/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  SwiftUI intro
+//
+//  Created by Dallin J Cottam on 9/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TextView()
+        TextView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI intro/SwiftUI intro/SwiftUI_introApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_introApp.swift
+//  SwiftUI intro
+//
+//  Created by Dallin J Cottam on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_introApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
