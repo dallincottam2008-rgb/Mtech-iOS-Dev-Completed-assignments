5ddc1c1501dae27f546d97da4cb2d0811d4b7436
Dallin J Cottam <dallinc@Dallins-MacBook-Pro.local>
2025-10-03 08:14:36 -0600

    Did sf 37

--- CodeChallenges/1 - Swift Fundamentals/SF37 - 'Selection Sort' - Sorting Algorithms.playground/Contents.swift
+++ CodeChallenges/1 - Swift Fundamentals/SF37 - 'Selection Sort' - Sorting Algorithms.playground/Contents.swift
@@ -6,6 +6,9 @@
     //  Create a function that takes in an array of Ints. Sort the array using an selection sort
     //  A visualization of what an insertion sort is can be seen here: 🔗https://en.wikipedia.org/wiki/Selection_sort#/media/File:Selection-Sort-Animation.gif
 
+//    return returnArray
+
+
 //  Examples:
     //  Input: [-43, 6, -2, 29, 16]
     //  Output: [-43, -2, 6, 16, 29]
@@ -15,3 +18,30 @@
     //  Like before, you are not expected to understand every element of the page, but take notes in a comment on this challenge about what you can glean from the information.
 
 import Foundation
+
+func selectionSort(_ array: [Int]) -> [Int] {
+    var returnArray: [Int] = []
+    var totalCount = array.count
+    var usedArray = array
+    var minNum = 0
+    var minNumIndex = 0
+    
+    for index in 0..<totalCount{
+        for (index, num) in usedArray.enumerated() {
+            if index == 0 {
+                minNum = num
+            } else if num <= minNum {
+                minNum = num
+                minNumIndex = index
+            }
+            print(minNum, returnArray)
+        }
+        usedArray.remove(at: minNumIndex)
+        returnArray.append(minNum)
+        minNum = 0
+        minNumIndex = 0
+        print(returnArray)
+    }
+    return returnArray
+}
+selectionSort([-43,6,-2,29,16])
