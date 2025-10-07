0825bec5d0e3ea8e12cad5d0007dc74248cc795d
Dallin J Cottam <dallinc@Dallins-MacBook-Pro.local>
2025-10-07 07:58:31 -0600

    Did tp 01

--- CodeChallenges/2 - Tables and Persistence/TP01 - 'Reverse Alphabetical' - Array Methods.playground/Contents.swift
+++ CodeChallenges/2 - Tables and Persistence/TP01 - 'Reverse Alphabetical' - Array Methods.playground/Contents.swift
@@ -3,7 +3,11 @@
 
 //  Instructions:
     //  Alphabetically reverse sort an array of strings.
+func reverseSort(_ array: [String]) -> [String] {
+    array.sorted().reversed()
+}
 
+print(reverseSort(["Bob", "Jill", "Sally"]))
 //  Examples:
     //  Input: ["Bob", "Jill", "Sally"], Output: ["Sally", "Jill", "Bob"]
     //  Input: ["Vern", "Gidget", "Zeb"], Output: ["Zeb", "Vern", "Gidget"]
@@ -12,5 +16,15 @@
     //  Create another function. This time, sort every word's letters reverse alphabetically, then sort all words in the array. You do not need to retain the case of the characters.
     //  Input: ["Bob", "Jill", "Sally"]
     //  Output: ["llji", "obb", "yslla"]
-
+//func blackDiamand(_ array: [String]) -> [String] {
+//    var sortArray = array
+//    
+//    for word in 0..<sortArray.count {
+//        let sortedWord = String(sortArray.lowercased().sorted(by: >))
+//    }
+//    
+//    return sortArray
+//}
+//
+//print(blackDiamand(["Bob", "Jill", "Sally"]))
 import Foundation
